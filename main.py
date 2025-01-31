fcb83ed92ce7f196b9e5b867e07f91f03522e809
D311154 <d311154@edu.curio.nl>
2025-01-31 11:13:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,22 +18,42 @@ while running:
     os.system('cls')
     print("Rollercoaster-check™")
     age = input("Voer leeftijd in: ")
-    height = input("Voer lengte in (in CM): ")
+    height = input("Voer lengte in CM in: ")
     age = int(age)
     height = int(height)
+    ageMissing = age_check - age
+    heightMissing = height_check - height
 
     #Process checks
-    if(age > age_check and height > height_check):
+    if(age >= age_check and height >= height_check):
         os.system('cls')
         print("Stap maar in!")
         print(coasterimg.get())
-        time.sleep(2)
+        time.sleep(3)
+
+    elif(age >= age_check and height < height_check):
+        os.system('cls')
+        print("je bent helaas te kort om in deze achtbaan te gaan...")
+        print(coasterimg.sad())
+        time.sleep(1)
+        print(f"als je hier in wilt, moet je nog {heightMissing}cm groeien!")
+        time.sleep(3)
+
+    elif(age < age_check and height >= height_check):
+        os.system('cls')
+        print("je bent helaas te jong om in deze achtbaan te gaan...")
+        print(coasterimg.sad())
+        time.sleep(1)
+        print(f"als je hier in wilt, moet je nog {ageMissing} jaar ouder worden!")
+        time.sleep(3)
 
     else:
         os.system('cls')
-        print("Je voldoet niet aan de voorwaarden...")
+        print("je bent te kort en te jong om in deze achtbaan te gaan...")
         print(coasterimg.sad())
-        time.sleep(2)
+        time.sleep(1)
+        print(f"als je hier in wilt, moet je nog {heightMissing}cm groeien en {ageMissing} jaar ouder worden!")
+        time.sleep(3)
 
     result = input("Druk op Enter om nog een keer te checken, of X om te stoppen\n\n")
     if(result.upper() == "X"):
